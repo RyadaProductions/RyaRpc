d9683f6134cb08685d3bf17a75fd076316b13460
Unknown <rodericks@live.nl>
2018-03-14 22:47:37 +0100

    Resolved issue #1, #2 and #3

--- RyaRpc/ViewModels/TitleBarViewModel.cs
+++ RyaRpc/ViewModels/TitleBarViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using Stylet;
 
 namespace RyaRpc.ViewModels
@@ -14,12 +15,18 @@ namespace RyaRpc.ViewModels
         private readonly IWindowManagerConfig _windowManagerConfig;
 
         /// <summary>
+        /// ViewModel of the trayicon
+        /// </summary>
+        private readonly TrayIconViewModel _trayIconViewModel;
+
+        /// <summary>
         /// TitleBar user control, capable of moving and closing the window
         /// </summary>
         /// <param name="windowManagerConfig"></param>
-        public TitleBarViewModel(IWindowManagerConfig windowManagerConfig)
+        public TitleBarViewModel(IWindowManagerConfig windowManagerConfig, TrayIconViewModel trayIconViewModel)
         {
             _windowManagerConfig = windowManagerConfig;
+            _trayIconViewModel = trayIconViewModel;
         }
 
         /// <summary>
@@ -30,7 +37,16 @@ namespace RyaRpc.ViewModels
             var window = _windowManagerConfig.GetActiveWindow();
             window.DragMove();
         }
-        
+
+        /// <summary>
+        /// Method invoked when the user clicks the minimize button on the window.
+        /// This hides the window from the user but keeps it running in the background.
+        /// </summary>
+        public void MinimizeApplication()
+        {
+            _trayIconViewModel.IsShellVisible = Visibility.Hidden;
+        }
+
         /// <summary>
         /// Close the application gracefully. 
         /// We close the Rpc thread gracefully here by calling RpcClient.Shutdown();
---
+++ RyaRpc/ViewModels/TrayIconViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using Stylet;
+
+namespace RyaRpc.ViewModels
+{
+    public class TrayIconViewModel : Screen
+    {
+        private Visibility _isShellVisible = Visibility.Visible;
+        public Visibility IsShellVisible
+        {
+            get => _isShellVisible;
+            set
+            {
+                if (SetAndNotify(ref _isShellVisible, value))
+                    NotifyOfPropertyChange(nameof(CanShowWindowCommand));
+            }
+        }
+
+        /// <summary>
+        /// Checks if the window is visible and returns false when it isn't. This is used to disable the show option in the context menu.
+        /// </summary>
+        public bool CanShowWindowCommand => IsShellVisible != Visibility.Visible;
+
+        /// <summary>
+        /// Method invoked from the tray icon, sets the current visibility of the shellview to visible
+        /// </summary>
+        public void ShowWindowCommand()
+        {
+            IsShellVisible = Visibility.Visible;
+        }
+
+        /// <summary>
+        /// Method intended to shutdown the application when the user presses exit in the tray icon
+        /// </summary>
+        public void ExitApplicationCommand()
+        {
+            Application.Current.Shutdown(0);
+        }
+    }
+}
